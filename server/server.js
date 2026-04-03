33eab22deeea36a8ad9300ae904b4e9179785a94
Anish Jrall <anishjrall@gmail.com>
2026-04-03 23:02:12 +0530

    frontend deploy

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use("/api/cart", require("./routes/cartRoutes"));
 const start = async () => {
   await connectDB();
 
-  const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-};
 
 start();
